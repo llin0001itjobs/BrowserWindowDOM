518b7a57a70148b5ca86e025dca22807cd70e5b9
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2025-05-30 16:49:14 -0400

    5 Changes
    5 Changes

--- src/main/java/org/llin/demo/browserDOM/BrowserWindowDomApplication.java
+++ src/main/java/org/llin/demo/browserDOM/BrowserWindowDomApplication.java
@@ -2,9 +2,16 @@ package org.llin.demo.browserDOM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BrowserWindowDomApplication {
+public class BrowserWindowDomApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BrowserWindowDomApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrowserWindowDomApplication.class, args);
