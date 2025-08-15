a7bd7840b4cb57d355b06ff59b291904e31f7fe5
Linus Lin <42655332+llin0001itjobs@users.noreply.github.com>
2025-08-15 17:00:25 -0400

    Add files via upload

--- src/test/java/org/llin/demo/browserDOM/BrowserWindowDomApplicationTests.java
+++ src/test/java/org/llin/demo/browserDOM/BrowserWindowDomApplicationTests.java
@@ -1,12 +1,7 @@
 package org.llin.demo.browserDOM;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BrowserWindowDomApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
